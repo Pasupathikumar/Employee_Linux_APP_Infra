c33ab91ab5bf08c7b82669003d1c1b71a25dbf89
Pasupathikumar <kumarmarimuthu99@gmail.com>
2026-09-09 04:56:34 +0530

    changed the vnet and subnet format in single resoruce block

--- terraform/vnet.tf
+++ terraform/vnet.tf
@@ -14,20 +14,17 @@ locals {
 }
 
 resource "azurerm_virtual_network" "vm_vnet_01" {
-    for_each = { for vnet in local.flatten_vnet_details : "${vnet.vnet_name}-${vnet.location}-${vnet.instace}" => vnet }
+    for_each = { for vnet in local.flatten_vnet_details : "${each.value.vnet_name}-${var.environment}-${var.project}-${each.value.location}-${each.value.instace}" => vnet }
 
     name                = "${each.value.vnet_name}-${var.environment}-${var.project}-${each.value.location}-${each.value.instace}"
     address_space       = each.value.vnet_address_space
     location            = each.value.location
     resource_group_name = azurerm_resource_group.vm_rg_01.name
     tags                = var.tags
+
+    subnet {
+        name             = "${each.value.subnet_name}-${var.environment}-${var.project}-${each.value.location}-${each.value.instace}"
+        address_prefixes = each.value.subnet_address_space
+    }
 }
 
-resource "azurerm_subnet" "vm_subnet_01" {
-    for_each = { for vnet in local.flatten_vnet_details : "${vnet.subnet_name}-${vnet.location}-${vnet.instace}" => vnet }
-    
-    name                 = "${each.value.subnet_name}-${var.environment}-${var.project}-${each.value.location}-${each.value.instace}"
-    resource_group_name  = azurerm_resource_group.vm_rg_01.name
-    virtual_network_name = azurerm_virtual_network.vm_vnet_01["${each.value.vnet_name}-${var.environment}-${var.project}-${each.value.location}-${each.value.instace}"].name
-    address_prefixes     = each.value.subnet_address_space
-}
